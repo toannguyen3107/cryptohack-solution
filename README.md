a20c8a00eaabc590699b725d24bc8b723c6ff805
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-09-15 19:21:49 +0700

    v1 test

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # solution crytohack
+<h1>This is a place that store some code practice cryptohack website!</h1>
--- quarictic.py
+++ quarictic.py
@@ -4,7 +4,7 @@ def solve(n):
     sq_root = int(math.sqrt(n))
     return (sq_root * sq_root) == n
 for i in range(0, 1111):
-    x = 14+ 29 * i
+    x = 14+29*i
     y = 6+29*i
     z = 11+29*i
     if solve(x) == True:
